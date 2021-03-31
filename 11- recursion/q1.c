39a3c7551c2996d6bcbd6536bd1b8e5a6e0740df
José Rogério <joserogerio.net@gmail.com>
2021-03-31 11:35:16 +0000

    q1 debug

--- 11- recursion/q1.c
+++ 11- recursion/q1.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+int r;
 int somaDigitos(int n) {
-	if (n == 0)
-		return 0;
-	else
-		return ( n%10 + somaDigitos(n/10));
+
+    if (n == 0)
+        return 0;
+    return ( n%10 + somaDigitos(n/10));
 }
 
-
-
 int main () {
-	int d;
-	scanf("%d", &d);
-	printf ("%d",somaDigitos(d));
-	return 0;
+    int d;
+    scanf("%d", &d);
+    r = somaDigitos(d);
+
+    if (r < 0)
+        r *= -1;
+
+    printf ("%d",r);
+    return 0;
 }
